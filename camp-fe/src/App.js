514726401198737c7edd6ed4683211572410434d
HaoQQQQ <c0989167331@gmail.com>
2022-02-15 17:06:13 +0800

    20220215

---
+++ camp-fe/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import CampDetail from './components/CampDetail'
+import Footer from './components/Footer';
+import Test from './components/Test'
+
+function App() {
+  return (
+    <>
+    {/* <NavBar /> */}
+      {/* <CampDetail /> */}
+      {/* <Footer /> */}
+      <Test />
+    </>
+  );
+}
+
+export default App;
