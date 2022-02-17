8a7ac74e9cee8d10c9a9b66687c532dbcbfb5300
HaoQQQQ <c0989167331@gmail.com>
2022-02-17 20:39:40 +0800

    20220217

--- camp-fe/src/App.js
+++ camp-fe/src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import CampDetail from './components/CampDetail'
 import Footer from './components/Footer';
-import Test from './components/Test'
+import Test2 from './components/Test2';
+import Booking from './components/Booking';
+import BookingSum from './components/BookingSum'
+
 
 function App() {
   return (
@@ -10,7 +13,10 @@ function App() {
     {/* <NavBar /> */}
       {/* <CampDetail /> */}
       {/* <Footer /> */}
-      <Test />
+      {/* <Booking /> */}
+      {/* <BookingSum /> */}
+      <Test2/>
+
     </>
   );
 }
