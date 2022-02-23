eb48a4215e35be980a292061ec4e418a8a43df93
hao <95270561+HaoQQQQ@users.noreply.github.com>
2022-02-23 20:31:09 +0800

    20220223

--- camp-fe/src/App.js
+++ camp-fe/src/App.js
@@ -1,20 +1,52 @@
 import React from "react";
-import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import "./style/App.css";
+
+// import Navbar from "./components/navbar/navbar";
+// import Navbar from "./components/Navbar";
+// import Home from "./pages/home";
+// import Products from "./components/Products";
+// import ProductDetail from "./components/ProductDetail";
+// import ShoppingCart from "./components/ShoppingCart";
+// import PaymentForGoods from "./components/PaymentForGoods";
+// import ShipmentCreditCard from "./components/ShipmentCreditCard";
+// import ShipmentConvenienceStore from "./components/ShipmentConvenienceStore";
+// import OrderSuccess from "./components/OrderSuccess";
+// import Footer from "./components/Footer";
+// import Home from "./pages/home";
 import CampDetail from "./components/CampDetail";
-import Footer from "./components/Footer";
-import Test2 from "./components/Test2";
 import Booking from "./components/Booking";
-import BookingSum from "./components/BookingSum";
+import PicSlider from "./components/PicSlider";
 
 function App() {
   return (
     <>
-      {/* <NavBar /> */}
-      {/* <CampDetail /> */}
-      {/* <Footer /> */}
-      <Booking />
-      <BookingSum />
-      {/* <Test2 /> */}
+      <div className="App">
+        <BrowserRouter>
+          {/* <Navbar /> */}
+          <Routes>
+            <Route path="/" exact element={<PicSlider />} />
+            {/* <Route path="/products" element={<Products />} /> */}
+            {/* <Route path="/products/:productId" element={<ProductDetail />} /> */}
+            {/* <Route path="/camp/:campId" element={<CampDetail />} /> */}
+
+            {/* <Route path="/p_orders/cart" element={<ShoppingCart />} /> */}
+            {/* <Route path="/p_orders/payment" element={<PaymentForGoods />} /> */}
+            <Route
+              path="/p_orders/shipment/credit_card"
+              // element={<ShipmentCreditCard />}
+            />
+            <Route
+              path="/p_orders/shipment/convenience_store"
+              // element={<ShipmentConvenienceStore />}
+            />
+            {/* <Route path="/p_orders/success" element={<OrderSuccess />} /> */}
+          </Routes>
+          {/* <Footer /> */}
+          {/* <ProductDetail /> */}
+        </BrowserRouter>
+      </div>
     </>
   );
 }
